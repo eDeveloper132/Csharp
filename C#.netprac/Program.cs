ed985c2d7994630a005260899dd99bbc58619f6f
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-02-02 01:03:44 +0500

    Add project files.

---
+++ C#.netprac/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_.netprac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            Console.WriteLine("Enter Your Name");
+            string UserName = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Age");
+            int UserAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("***Ans***");
+            Console.WriteLine("User Name is ");
+            Console.WriteLine(UserName);
+            Console.WriteLine("User Age is ");
+            Console.WriteLine(UserAge);
+            Console.ReadLine();
+
+
+        }
+    }
+}
